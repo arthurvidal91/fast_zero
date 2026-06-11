89d0f8375aefef929666c4262d0aeeceaca5823c
Arthur Vidal <arthurvidal@192.168.0.14>
2026-06-11 09:34:27 -0300

    chore: adding new resource and tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,7 +9,18 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     client = TestClient(app)
 
-    response = client.get("/")
+    response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'messages': 'Olá, Mundo!'}
+    assert response.json() == {'message': 'Olá, Mundo!'}
+
+
+def test_root_html_retornar_ok():
+
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.headers["content-type"].startswith("text/html")
+    assert "<h1> Olá, Mundo!</h1>" in response.text
